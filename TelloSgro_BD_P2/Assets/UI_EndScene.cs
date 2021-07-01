c36b7555f9d779dd2fe7feb87674e6dd5573c301
Adrian <Owinmowe@gmail.com>
2021-07-01 05:43:38 -0300

    Added show your highscore at the end of gameplay.

--- TelloSgro_BD_P2/Assets/UI_EndScene.cs
+++ TelloSgro_BD_P2/Assets/UI_EndScene.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_EndScene : MonoBehaviour
 {
     [SerializeField] List<UI_Component> endScene_HUD = null;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     string handlerText;
 
@@ -22,6 +24,8 @@ public class UI_EndScene : MonoBehaviour
         {
             item.TransitionIn();
         }
+        SessionData currentData = LoaderManager.Get().GetSessionData();
+        scoreText.text = currentData.username + "\n Score: " + currentData.score + " Deaths: " + currentData.deaths;
     }
 
     public void BackToMenu()
