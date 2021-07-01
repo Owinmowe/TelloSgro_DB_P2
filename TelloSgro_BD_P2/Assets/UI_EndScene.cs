ae083db7c8a362b414a3d358c9db5376e41aa64e
Adrian <Owinmowe@gmail.com>
2021-07-01 06:31:39 -0300

    Added high score ranking texts.

--- TelloSgro_BD_P2/Assets/UI_EndScene.cs
+++ TelloSgro_BD_P2/Assets/UI_EndScene.cs
@@ -6,9 +6,10 @@ using TMPro;
 public class UI_EndScene : MonoBehaviour
 {
     [SerializeField] List<UI_Component> endScene_HUD = null;
-    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] List<TextMeshProUGUI> scoreTexts = null;
+    [SerializeField] TextMeshProUGUI scoreText = null;
 
-    string handlerText;
+    List<SessionData> rankingData = new List<SessionData>();
 
     private void Awake()
     {
@@ -29,7 +30,21 @@ public class UI_EndScene : MonoBehaviour
 
     void SetHighScore(string scoreText)
     {
-        Debug.Log(handlerText);
+        rankingData = new List<SessionData>();
+        var newString = scoreText;
+        var HighScoreStringArray = newString.Split('_');
+        for (int i = 0; i < HighScoreStringArray.Length; i++)
+        {
+            var scoreComponent = HighScoreStringArray[i].Split('-');
+            rankingData[i].username = HighScoreStringArray[0];
+            rankingData[i].deaths = int.Parse(HighScoreStringArray[1]);
+            rankingData[i].score = int.Parse(HighScoreStringArray[2]);
+        }
+
+        for (int i = 0; i < rankingData.Count; i++)
+        {
+            scoreTexts[i].text = rankingData[i].username + " " + rankingData[i].deaths + " " + rankingData[i].score;
+        }
     }
 
     public void BackToMenu()
